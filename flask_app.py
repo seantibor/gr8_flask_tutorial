69b35da8e91d0f60e769e28b813fcf12e2f73485
Sean Tibor <sean.tibor@pinecrest.edu>
2020-01-08 19:10:49 +0000

    end of day 2

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,1 @@
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
-import random
-
-students = ['Alison','George', 'Jonah','Jake','Peter','Tyler','Savannah','Aviah','Taylor','Ellie','Shaun','Aiden']
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    student = random.choice(students)
-    return render_template("home.html", student=student)
+from app import app
